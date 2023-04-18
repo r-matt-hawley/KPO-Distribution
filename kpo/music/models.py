22dd823cf0824a07ff9c5827ed1ef36eb5e62ca2
Matt Hawley <r.matt.hawley@gmail.com>
2023-04-18 18:33:30 -0500

    init Models

--- kpo/music/models.py
+++ kpo/music/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+class Season(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str___(self):
+        return self.name
+
+
+class Concert(models.Model):
+    title = models.CharField(max_length=200)
+    season = models.ForeignKey(Season)
+    performance_date = models.DateTimeField("Performance Date")
+
+    def __str___(self):
+        return f"{self.season} {self.performance_date.year}, {self.title}"
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    concert = models.ManyToManyField(Concert, related_name="songs")
+
+    def __str___(self):
+        return self.title
+
+
+class Part(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str___(self):
+        return self.name
+
+
+class File(models.Model):
+    title = models.CharField(max_length=200) # TODO: Is this needed?
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    part = models.ForeignKey(Part, on_delete=models.CASCADE)
+    created_dt = models.DateTimeField("Created Date")
+    modified_dt = models.DateTimeField("Last Modified", auto_now=True)
+
+    def __str___(self):
+        return f"{self.song}/{self.part}"
+
