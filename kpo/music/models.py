020d1ea8a7c4073a8932592828e31529df7bb96d
Matt Hawley <r.matt.hawley@gmail.com>
2023-04-19 18:37:56 -0500

    Add Performance Table; Add Docstrings

--- kpo/music/models.py
+++ kpo/music/models.py
@@ -1,43 +1,59 @@
 from django.db import models
+    
+class File(models.Model):
+    """One page of a Part"""
+    url = models.FilePathField() #TODO: How to define FilePathField
+
+    # TODO: Can we get the number of pages in a PDF programmatically?
+    page_num = models.IntegerField("Page Number")
+    created_dt = models.DateTimeField("Created Date", auto_now_add=True)
+    modified_dt = models.DateTimeField("Last Modified", auto_now=True)
+
+    def __str___(self):
+        return self.url
+
+
+class Part(models.Model):
+    """Instrument part for a Song"""
+    name = models.CharField(max_length=20)
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+
+    def __str___(self):
+        return self.name
+
+
+class Song(models.Model):
+    """Single song in a Concert"""
+    title = models.CharField(max_length=200)
+    part = models.ForeignKey(Part, on_delete=models.CASCADE)
+
+    def __str___(self):
+        return self.title
+
 
 class Concert(models.Model):
+    """Set of Performaces for a certain set of Songs"""
 
     class Season(models.IntegerChoices):
+        """Choices for Concert.season"""
         FALL = 1
         CHRISTMAS = 2
         FEBRUARY = 3
         SPRING = 4
 
     title = models.CharField(max_length=200)
-    season = models.IntegerField(choices=Season.choices, )
-    performance_date = models.DateTimeField("Performance Date")
-
-    def __str___(self):
-        return f"{self.season} {self.performance_date.year}, {self.title}"
-
-
-class Song(models.Model):
-    title = models.CharField(max_length=200)
-    concert = models.ManyToManyField(Concert, related_name="songs")
+    season = models.IntegerField(choices=Season.choices) # Not confused with "Concert Season" like 2022-2023
+    song = models.ManyToManyField(Song, related_name="concerts")
 
     def __str___(self):
         return self.title
 
 
-class Part(models.Model):
-    name = models.CharField(max_length=20)
+class Performance(models.Model):
+    """Performance date for a concert"""
 
-    def __str___(self):
-        return self.name
+    performance_date = models.DateTimeField("Performance Date")
+    concert = models.ForeignKey(Concert, on_delete=models.CASCADE)
 
-
-class File(models.Model):
-    title = models.CharField(max_length=200) # TODO: Is this needed?
-    song = models.ForeignKey(Song, on_delete=models.CASCADE)
-    part = models.ForeignKey(Part, on_delete=models.CASCADE)
-    created_dt = models.DateTimeField("Created Date")
-    modified_dt = models.DateTimeField("Last Modified", auto_now=True)
-
-    def __str___(self):
-        return f"{self.song}/{self.part}"
-
+    def __str__(self):
+        return self.performance_date
