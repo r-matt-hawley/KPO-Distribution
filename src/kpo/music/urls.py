8f2112034378cf33c1bcc6d36be5ac45ac089258
Matt Hawley <r.matt.hawley@gmail.com>
2023-06-02 15:27:44 -0500

    Change url part/upload to file/upload

--- src/kpo/music/urls.py
+++ src/kpo/music/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.index),
     path("download/", views.download, name="download"),
     path("view_list/", views.view_list, name="view_list"),
-    path("part/upload", views.FileUploadView, name="FileUploadView"),
+    path("file/upload", views.FileUploadView, name="FileUploadView"),
     path("Parts/", views.Parts, name="Parts"),
     path("Parts/<int:partid>", views.PartID, name="PartID"),
 ]
