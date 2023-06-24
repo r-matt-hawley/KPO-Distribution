25780b70927c798ca04e82896222b8305ac0c716
Matt Hawley <r.matt.hawley@gmail.com>
2023-06-24 17:29:08 -0500

    Fix URLs must include <int:pk> when using CBV

--- src/kpo/music/urls.py
+++ src/kpo/music/urls.py
@@ -5,15 +5,58 @@ from . import views
 app_name = "music"
 
 urlpatterns = [
-    path("concerts", views.ConcertListView.as_view(), name="concert_list"),
-    path("concerts/create/", views.ConcertCreateView.as_view(), name="concert_create"),
-    path("concerts/<int:concert_pk>/", views.ConcertDetailView.as_view(), name="concert_detail"),
-    path("concerts/<int:concert_pk>/delete/", views.ConcertDeleteView.as_view(), name="concert_delete"),
-    path("concerts/<int:concert_pk>/update/", views.ConcertUpdateView.as_view(), name="concert_update"),
-    path("concerts/<int:concert_pk>/songs/create/", views.SongCreateView.as_view(), name="song_create"),
-    path("concerts/<int:concert_pk>/songs/<int:song_pk>/", views.SongDetailView.as_view(), name="song_detail"),
-    path("concerts/<int:concert_pk>/songs/<int:song_pk>/update/", views.SongUpdateView.as_view(), name="song_update"),
-    path("concerts/<int:concert_pk>/songs/<int:song_pk>/delete/", views.SongDeleteView.as_view(), name="song_delete"),
+    path("concerts", 
+        views.ConcertListView.as_view(), 
+        name="concert_list"),
+
+    path("concerts/create/", 
+        views.ConcertCreateView.as_view(), 
+        name="concert_create"),
+
+    path("concerts/<int:pk>/", 
+        views.ConcertDetailView.as_view(), 
+        name="concert_detail"),
+
+    path("concerts/<int:pk>/delete/", 
+        views.ConcertDeleteView.as_view(), 
+        name="concert_delete"),
+
+    path("concerts/<int:pk>/update/", 
+        views.ConcertUpdateView.as_view(), 
+        name="concert_update"),
+
+    path("concerts/<int:pk>/songs/create/",
+        views.SongCreateView.as_view(), 
+        name="song_create"),
+
+    path("concerts/<int:concert_pk>/songs/<int:pk>/",
+        views.SongDetailView.as_view(), 
+        name="song_detail"),
+
+    path("concerts/<int:concert_pk>/songs/<int:pk>/update/", 
+        views.SongUpdateView.as_view(), 
+        name="song_update"),
+
+    path("concerts/<int:concert_pk>/songs/<int:pk>/delete/", 
+        views.SongDeleteView.as_view(), 
+        name="song_delete"),
+
+    path("concerts/<int:concert_pk>/songs/<int:pk>/parts/create/", 
+        views.PartCreateView.as_view(), 
+        name="part_create"),
+
+    path("concerts/<int:concert_pk>/songs/<int:song_pk>/parts/<int:pk/", 
+        views.PartDetailView.as_view(), 
+        name="part_detail"),
+
+    path("concerts/<int:concert_pk>/songs/<int:song_pk>/parts/<int:pk/update/", 
+        views.PartUpdateView.as_view(), 
+        name="part_update"),
+
+    path("concerts/<int:concert_pk>/songs/<int:song_pk>/parts/<int:pk/delete/", 
+        views.PartDeleteView.as_view(), 
+        name="part_delete"),
+
     path("download/", views.download, name="download"),
     path("view_list/", views.view_list, name="view_list"),
     path("file/upload", views.FileUploadView, name="FileUploadView"),
