2978f0d43505f83c68716d778a5fe63d6da98e13
Matt Hawley <r.matt.hawley@gmail.com>
2024-02-19 22:30:00 -0600

    use os.path.join

--- src/kpo_dist_api/config.py
+++ src/kpo_dist_api/config.py
@@ -2,12 +2,13 @@ import pathlib
 from connexion import FlaskApp
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from os.path import join
 
 basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = FlaskApp(__name__, specification_dir=basedir)
 
 app = connex_app.app
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'db/music.db'}"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{join(basedir, 'db', 'music.db')}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
